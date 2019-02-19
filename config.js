d73d9c803fdfd0e9ffa7ddb7bdbc42ad0c77f705
Menella Erwan <erwan.menella@hotmail.fr>
2019-02-19 04:27:55 +0100

    First Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+token: '', // token of bot
+idbot: '', //ID client
+idchannel: '', // ID channel where Pointage run
+manager: '', // ID user for alert
+time: '09:30' //Format HH:MM
+};
+module.exports = config;
---
+++ main.js
@@ -0,0 +1,44 @@
+//Require
+const config = require('./config.js');
+const Discord = require('discord.js');
+var schedule = require('node-schedule');
+
+//Set objet,time,config
+const {token,idbot,idchannel,manager,time} = config;
+const client = new Discord.Client();
+var d = new Date();
+
+// Variable
+var jour = d.getDate();
+var mois = d.getMonth() + 1;
+var splitTime = time.split(':');
+var setHour = splitTime[0];
+var setMinute = splitTime[1];
+
+  client.on('ready',() => {
+    client.user.setPresence({ game: { name: 'Created by BM' }, status: '@ErwanMenella' });
+    var j =schedule.scheduleJob({hour: setHour,minute: setMinute}, function(){
+    client.channels.get(idchannel).send(`@CS:GO Pointage du ${jour}/${mois} pour les entraînements globaux de 20h à 23h.`);
+  });
+   
+})
+  
+client.on('message',(message) => {
+  if (message.channel.id==idchannel && message.author.id ==idbot) {
+    message.react("📗")
+    .then(message.react("📕"));
+  }
+
+})
+
+client.on('messageReactionAdd',(reaction,user) => {
+  if (reaction.message.channel.id==idchannel && reaction.message.author.id==idbot && user.id!==idbot && reaction.message.id==reaction.message.channel.lastMessageID ) {
+    var username = user.id;
+    if (reaction.emoji.name=="📗") {
+      var status = "présent";
+    }else var status = "absent";
+    client.users.get(manager).send(`<@${username}> sera ${status}.`);
+  }
+})
+
+client.login(token);
