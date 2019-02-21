584cae791ba7875e11c78d32b5a4d7bfbc624373
Menella Erwan <erwan.menella@hotmail.fr>
2019-02-21 17:47:45 +0100

    beta

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ token: '', // token of bot
 idbot: '', //ID client
 idchannel: '', // ID channel where Pointage run
 manager: '', // ID user for alert
-time: '09:30' //Format HH:MM
+time: '09:30', //Format HH:MM
+message: '@CS:GO Pointage du ${jour}/${mois} pour les entraînements globaux de 20h à 23h.'
 };
 module.exports = config;
