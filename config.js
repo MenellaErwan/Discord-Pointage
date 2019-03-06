03ee2935c5fcf0c382c0c914db31c661688db4ce
Menella Erwan <erwan.menella@hotmail.fr>
2019-03-06 19:44:03 +0100

    fix

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-token: 'NTUyOTE2MzQ5MjU3Nzc3MTYy.D2GfAw.J7qfClrjblLQu4Q-WymiMuNBY8Q', // token of bot
-idbot: '552916349257777162', //ID client
-idchannel: '436950886120554498', // ID channel where Pointage run
-manager: '196297517317619712', // ID user for alert
-captain: '196297517317619712',
-mentionid: '444903954829017088',
-time: '19:42' //Format HH:MM
+token: '', // token of bot
+idbot: '', //ID client
+idchannel: '', // ID channel where Pointage run
+manager: '', // ID user for alert
+captain: '',
+mentionid: '',
+time: '9:30' //Format HH:MM
 };
 module.exports = config;
